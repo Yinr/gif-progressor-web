d3b6fd4ed15fbe26399e516b914d78b6a83be145
Yinr <yinr@yinr.cc>
2019-11-07 16:14:36 +0800

    fixup: allow file extension with upper case

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=["GET"])
